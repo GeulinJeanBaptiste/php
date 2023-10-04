963d378401a25bebcccd080f33d0ae26ebab4ea2
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-04 10:32:33 +0200

    Initialisation php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include('./header.php');
+
+
+echo "<h1>CEPPIC PHP</h1>";
+
+?>
+<!-- Le retour PHP du serveur est toujours en html. -->
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque labore deleniti modi consequatur, ab aliquam rerum deserunt fuga quis amet in a qui veritatis assumenda asperiores sequi eveniet doloribus at! Aut, architecto error harum voluptatum odio dolorum ex magnam optio, animi, molestias sed voluptatibus nostrum nobis soluta perferendis impedit.</p>
+<?php
+include('./footer.php');
+echo "Encore PHP";
+?>
