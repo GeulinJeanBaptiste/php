e67d9233a16f3f5812a54bd41d7e974fa5b065b3
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-04 16:55:32 +0200

    tableaux

--- index.php
+++ index.php
@@ -6,5 +6,18 @@ require './fonction.php';
 $titre = "Tableaux php";
 
 
+$tab1 = [];
+$notes = [12,14,6,10];
+
+$tab1[] = 'Janvier';
+$tab1[] = 45;
+$tab1[] = true;
+
+array_push($tab1, 'Voiture', 10.5, [45,true,'Truc']);
+
+$tabFusion = array_merge($tab1,$notes);
+dbug($tabFusion);
+dd($tabFusion[5][2]);
+
 
 require './index.view.php';
