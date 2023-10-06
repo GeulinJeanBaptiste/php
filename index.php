e5364f7655324d6f8a9695ab9cb51fa6904c546d
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-06 16:54:56 +0200

    structure de controle

--- index.php
+++ index.php
@@ -3,53 +3,63 @@
 
 require './fonction.php';
 
-$titre = "Boucles php";
+$titre = "conditions php";
 
 
 
-/* $planetes = ['mars', 'terre', 'uranus', 'venus', 'jupiter', 'mercure'];
-$planetes2 = [
-    'm' => 'mars',
-    't' => 'terre',
-    'u' => 'uranus',
-    'v' => 'venus',
-    'j' => 'jupiter',
-    'm' => 'mercure'
-];
 
-dbug($planetes); */
 
-/* foreach ($planetes as $planete) {
-    // echo "$planetes<br>";
-    echo ucwords($planete) . '<br>';
-}
- */
-/* foreach ($planetes2 as $planete) {
-    // echo "$planetes<br>";
-    echo ucwords($planete) . '<br>';
-} */
-
-/* foreach ($planetes as $clef => $valeur) {
-    echo $clef . ' : ' . $valeur . '<br>';
-};
-foreach ($planetes2 as $clef => $valeur) {
-    echo $clef . ' : ' . $valeur . '<br>';
-};
-foreach ($planetes2 as $clef => $valeur) :
-    echo $clef . ' : ' . $valeur . '<br>';
-endforeach;
- */
 
 $user = [
     'nom' => 'Doupet',
     'email' => 'ldoupe@yaya.com',
     'competence' => ['php', 'react', 'python']
 ];
-dbug($user);
-//Afficher dans une liste UL toutes les données de ce user avec for each.
-foreach ($user as $key => $value) :
-    echo $key . ' : ' . $value;
-endforeach;
+
+/* if/else/elsif 
+/*
+>
+<
+>=
+<=
+== comparaison d'egalite en valeur.
+=== comparaison d'egalite en valeur et en type ( il doit être égale en tout).
+!= 
+!==
+fin du commentaire */
+
+/* $age = 20;
+if ($age >= 18) {
+    echo 'peut voter. ';
+} else {
+    echo 'Ne peut pas voter !!!';
+}
+
+if ($age >= 18) :
+    echo 'peut voter. ';
+else :
+    echo 'Ne peut pas voter !!!';
+endif; */
+
+/* $heure = 10;
+$heure = date('H');
+
+
+if ($heure <= 12) :
+    echo "On est le matin.";
+elseif ($heure >= 18) :
+    echo "On est le soir.";
+else :
+    echo "On est l'apres-midi.";
+endif; */
+
+
+$a = 20;
+$b = 5;
+
+if ($b === '5') :
+    echo 'La variable $b n\'est pas égale à 5.';
+endif;
 
 
 
