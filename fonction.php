a0cd0f81315f250a8b8ee5271bf61a80214ed7d0
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-12 14:20:08 +0200

    formulaire

--- fonction.php
+++ fonction.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 function dbug($value)
 {
-    echo '<pre style="background-color:black; color:white;overflow: auto;padding: 10px;">';
+    echo '<pre style="background-color:black; color:white;overflow: auto;font-family:monospace;padding: 1rem;">';
     print_r($value);
     // var_dump($tableau);
     echo '</pre>';
@@ -16,61 +16,3 @@ function dd($value)
     dbug($value);
     die('Script php arrété !!!');
 }
-
-/* function calcul($a = 10, $b = 12)
-{
-    // $c = $a + $b;
-    // return $c;
-    // echo 'Calcul';
-    return $a + $b;
-}
-
-echo calcul(0,10); */
-
-// Créer une fonction qui calcul le prix TTC en lui passant un prix HT et la TVA.
-// calculPrixTTC
-
-// Crer une fonction qui calcul l'age d'une personne en lui donnant sa date de naissance.
-
-/* function prixTTC($TVA, $HT)
-{
-    return $HT + ($HT * $TVA);
-}
-echo prixTTC(.20, 18); */
-
-/* function Age($naissance, $annee = 2023)
-{
-    return $annee - $naissance;
-}
-echo Age(1985); */
-
-
-/* function calculAge($anneeNaissance)
-{
-    $anneeEnCours = date("Y");
-    return $anneeEnCours -$anneeNaissance;
-}
-echo calculAge(1990); */
-
-/* $calcul = function ($nb) {
-    return $nb + 1;
-};
-echo $calcul(25); */
-
-/* function calcul(int | float $a, int | float $b): mixed
-{
-    return $a + $b;
-}
-echo calcul(5.6, 7); */
-
-$x = 5;
-$y = 3;
-
-function calcul($xPasseEnParametre)
-{
-    // global $y;
-    $y = 7;
-    return $xPasseEnParametre + $y;
-}
-echo calcul(5);
-dd($y);
--- index.php
+++ index.php
@@ -1,9 +1,55 @@
 <?php
-
-
 require './fonction.php';
+$titre = "Formulaire";
 
-$titre = "Fonctions";
+// dbug($_POST);
+// Afficher des phrases avec des informations du formulaire: 
+// votre nom est :
+// votre email est :
+// votre commentaire est :
+// votre niveau est :
+// vos competences sont :
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Formulaire</title>
+</head>
+
+<body>
+    <h1>formulaire</h1>
+    <form action="" method="POST">
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" required>
+        <label for="email">Email:</label>
+        <input type="email" name="email">
+        <label for="commentaire">Commentaire :
+            <textarea name="commentaire" id="commentaire" cols="30" rows="10">
+            </textarea>
+        </label>
+        <label for="niveau">Niveau :
+            <input type="radio" name="niveau" value="debutant">Débutant
+            <input type="radio" name="niveau" value="debutant">Intermediaire
+            <input type="radio" name="niveau" value="debutant">Expert
+        </label>
+        <label for="competence">
+            Competence :
+            PHP<input type="checkbox" name="competence[]" value="php">
+            Python<input type="checkbox" name="competence[]" value="Python">
+            CSS<input type="checkbox" name="competence[]" value="CSS">
+            Javascript<input type="checkbox" name="competence[]" value="Javascript">
+        </label>
+        <input type="submit" name="submitted" value="Valider">
+    </form>
+</body>
+
+</html>
 
 
 
@@ -14,8 +60,5 @@ $titre = "Fonctions";
 
 
 
-
-
-
-
+<?php
 require './index.view.php';
