003eefb9b70eb0099e12a76da260c712858b1339
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-12 11:30:38 +0200

    Fonctions qui calcul

--- fonction.php
+++ fonction.php
@@ -14,3 +14,38 @@ function dd($value)
     dbug($value);
     die('Script php arrété !!!');
 }
+
+/* function calcul($a = 10, $b = 12)
+{
+    // $c = $a + $b;
+    // return $c;
+    // echo 'Calcul';
+    return $a + $b;
+}
+
+echo calcul(0,10); */
+
+// Créer une fonction qui calcul le prix TTC en lui passant un prix HT et la TVA.
+// calculPrixTTC
+
+// Crer une fonction qui calcul l'age d'une personne en lui donnant sa date de naissance.
+
+/* function prixTTC($TVA, $HT)
+{
+    return $HT + ($HT * $TVA);
+}
+echo prixTTC(.20, 18); */
+
+/* function Age($naissance, $annee = 2023)
+{
+    return $annee - $naissance;
+}
+echo Age(1985); */
+
+
+/* function calculAge($anneeNaissance)
+{
+    $anneeEnCours = date("Y");
+    return $anneeEnCours -$anneeNaissance;
+}
+echo calculAge(1990); */
--- index.php
+++ index.php
@@ -3,101 +3,16 @@
 
 require './fonction.php';
 
-$titre = "conditions php";
+$titre = "Fonctions";
 
 
 
 
 
 
-$user = [
-    'nom' => 'Doupet',
-    'email' => 'ldoupe@yaya.com',
-    'competence' => ['php', 'react', 'python']
-];
-
-/* if/else/elsif 
-/*
->
-<
->=
-<=
-== comparaison d'egalite en valeur.
-=== comparaison d'egalite en valeur et en type ( il doit être égale en tout).
-!= 
-!==
-fin du commentaire */
-
-//  $age = 20;
-// if ($age >= 18) {
-//     echo 'peut voter. ';
-// } else {
-//     echo 'Ne peut pas voter !!!';
-// }
-// echo($age >=18) ? 'Peut voter' : 'Ne peut pas voter !'; 
-// if ($age >= 18) :
-//     echo 'peut voter. ';
-// else :
-//     echo 'Ne peut pas voter !!!';
-// endif; 
-
-/* $heure = 10;
-$heure = date('H');
 
 
-if ($heure <= 12) :
-    echo "On est le matin.";
-elseif ($heure >= 18) :
-    echo "On est le soir.";
-else :
-    echo "On est l'apres-midi.";
-endif; */
 
-
-/* $a = 20;
-$b = 5;
-
-if ($b === '5') :
-    echo 'La variable $b n\'est pas égale à 5.';
-endif; */
-
-/* $vrai = false;
-if ($vrai) :
-    echo 'Vrai';
-else :
-    echo 'Faux';
-endif; */
-
-/* echo $vrai ? 'Vrai' : 'Faux'; */
-
-// $votes = 7500;
-// dbug($votes);
-// $votesprécédents = 1254;
-// ($votes > $votesprécédents) ? $votes-- : $votes++;
-// dbug($votes);
-
-$couleur = 'violet';
-/* switch ($couleur) {
-    case 'rouge':
-        echo 'Votre couleur favorite est le rouge';
-        break;
-    case 'bleu':
-        echo 'Votre couleur favorite est le bleu';
-        break;
-    case 'vert':
-        echo 'Votre couleur favorite est le vert';
-        break;
-    default:
-        echo 'Votre couleur favorite est ni le rouge, ni le bleu, ni le vert';
-} */
-
-$return_value = match($couleur) {
-    'rouge' => 'Votre couleur prefere est le rouge',
-    'bleu' => 'Votre couleur prefere est le bleu',
-    'vert'=>'Votre couleur prefere est le vert',
-    default =>'Votre couleur favorite est ni le rouge, ni le bleu, ni le vert',
-};
-dbug($return_value);
 
 
 
