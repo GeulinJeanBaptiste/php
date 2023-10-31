f6175e1776c0be2003f5496ac64ddc6734463bde
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-31 11:51:34 +0100

    création de dossier admin et fichiers admin

---
+++ views/admin/index.view.php
@@ -0,0 +1,3 @@
+<?php require 'views/partials/header.php' ?>
+<h1>admin</h1>
+<?php require 'views/partials/footer.php' ?>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,5 +3,6 @@
         <li><a href="/">Acceuil</a></li>
         <li><a href="/notes">Notes</a></li>
         <li><a href="/contact">Contact</a></li>
+        <li><a href="/admin">Admin</a></li>
     </ul>
 </nav>
